af7debc84ab094043debd1da8c4f3766168ed77e
Carbon <carbon.szlachta@gmail.com>
2020-10-12 15:37:48 +0200

    zad7

---
+++ zad7.py
@@ -0,0 +1,18 @@
+number = -1
+while True:
+    number = int(input("Enter number: "))
+    if number < 1:
+        print("Must be > 0")
+    else:
+        break
+
+a = 1
+b = 1
+
+while a * b < number:
+    a, b = b, a + b
+
+if a * b == number:
+    print("No jest i fajnie")
+else:
+    print("Nie jest")
